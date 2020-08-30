98687aa5db0de40cda6137ac6a03f18d62f49e2a
Leo Driesch <leo.driesch@gmail.com>
2020-08-30 22:53:28 +0200

    fix(eslint-config): fix rule overrides for typescript files

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,26 @@
+const rules = {
+	"import/extensions": [
+		"error",
+		"always",
+		{ js: "never", jsx: "never", ts: "never", tsx: "never" },
+	],
+	"import/no-unresolved": ["error", { ignore: ["chayns-components"] }],
+	"import/prefer-default-export": "off",
+	"no-console": ["warn", { allow: ["warn", "error"] }],
+	"no-param-reassign": [
+		"error",
+		{ ignorePropertyModificationsFor: ["draft"], props: true },
+	],
+	"no-restricted-imports": [
+		"error",
+		{
+			message: "Use 'chayns-components' instead to enable tree-shaking.",
+			name: "chayns-components/lib",
+		},
+	],
+	"no-use-before-define": ["error", "nofunc"],
+}
+
 module.exports = {
 	env: {
 		browser: true,
@@ -5,28 +28,7 @@ module.exports = {
 	},
 	extends: ["airbnb", "airbnb/hooks", "prettier", "prettier/react"],
 	globals: { chayns: true },
-	rules: {
-		"import/extensions": [
-			"error",
-			"always",
-			{ js: "never", jsx: "never", ts: "never", tsx: "never" },
-		],
-		"import/no-unresolved": ["error", { ignore: ["chayns-components"] }],
-		"import/prefer-default-export": "off",
-		"no-console": ["warn", { allow: ["warn", "error"] }],
-		"no-param-reassign": [
-			"error",
-			{ ignorePropertyModificationsFor: ["draft"], props: true },
-		],
-		"no-restricted-imports": [
-			"error",
-			{
-				message: "Use 'chayns-components' instead to enable tree-shaking.",
-				name: "chayns-components/lib",
-			},
-		],
-		"no-use-before-define": ["error", "nofunc"],
-	},
+	rules,
 	settings: {
 		"import/resolver": {
 			node: { extensions: [".js", ".jsx", ".ts", ".tsx"] },
@@ -45,6 +47,7 @@ module.exports = {
 				"prettier/@typescript-eslint",
 				"prettier/react",
 			],
+			rules,
 			parserOptions: {
 				project: "./tsconfig.json",
 			},
