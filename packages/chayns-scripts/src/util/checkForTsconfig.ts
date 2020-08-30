b925e5ecc349a9670e50e030554d5eb1650fc321
Leo Driesch <leo.driesch@gmail.com>
2020-08-30 22:51:20 +0200

    feat(cli): add automatic creation of tsconfig

---
+++ packages/chayns-scripts/src/util/checkForTsconfig.ts
@@ -0,0 +1,34 @@
+import * as fs from "fs"
+import * as path from "path"
+import { defaultTsconfig } from "./defaultTsconfig"
+
+export function setupTsconfig(): void {
+	fs.writeFileSync(
+		path.resolve(process.cwd(), "tsconfig.json"),
+		JSON.stringify(defaultTsconfig, undefined, 4)
+	)
+
+	// eslint-disable-next-line no-console
+	console.log(
+		"\nIt seems like you want to use TypeScript in this project. I went ahead and set up a tsconfig.json file for you.\n\n"
+	)
+}
+
+export function checkForTsconfig(): boolean {
+	const hasTsconfig = fs.existsSync(
+		path.resolve(process.cwd(), "tsconfig.json")
+	)
+
+	if (hasTsconfig) {
+		const tsconfig = fs.readFileSync("tsconfig.json", "utf8")
+
+		const trimmedTsconfig = tsconfig.replace(/\s/g, "")
+
+		if (trimmedTsconfig === "" || trimmedTsconfig === "{}") {
+			setupTsconfig()
+			return true
+		}
+	}
+
+	return false
+}
