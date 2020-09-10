81a3a6fd36073476ed8ea68865abe2c3247fe75e
Leonhard Driesch <leo.driesch@gmail.com>
2020-09-10 16:56:24 +0200

    chore(eslint-config): disable often misregarded rules

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -22,6 +22,14 @@ const rules = {
 		"error",
 		{ functions: false, classes: false, variables: false },
 	],
+	"no-plusplus": "off",
+	"jsx-a11y/no-noninteractive-element-interactions": "off",
+	"jsx-a11y/no-static-element-interactions": "off",
+	"jsx-a11y/click-events-have-key-events": "off",
+	"jsx-a11y/heading-has-content": "off",
+	"jsx-a11y/anchor-has-content": "off",
+	"jsx-a11y/anchor-is-valid": "off",
+	"jsx-a11y/href-no-hash": "off",
 }
 
 module.exports = {
