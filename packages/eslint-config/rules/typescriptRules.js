f620d8241821dc593d256ae2b4bf548767b01728
Leo Driesch <leo.driesch@gmail.com>
2020-09-13 21:27:27 +0200

    fix(eslint-config): disable default props rule for typescript

--- packages/eslint-config/rules/typescriptRules.js
+++ packages/eslint-config/rules/typescriptRules.js
@@ -1,5 +1,6 @@
 const typescriptRules = {
 	"react/prop-types": "off",
+	"react/require-default-props": "off",
 	"@typescript-eslint/no-use-before-define": [
 		"error",
 		{ functions: false, classes: false, variables: false, enums: false },
