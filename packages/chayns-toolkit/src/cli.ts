1661e20079a16695868e47be9a4c98dfe8415893
Leonhard Driesch <leo.driesch@gmail.com>
2020-10-15 16:15:53 +0200

    :bug: Fix issues when bundling

--- packages/chayns-toolkit/src/cli.ts
+++ packages/chayns-toolkit/src/cli.ts
@@ -1,9 +1,8 @@
 import { Command } from "commander"
-import * as path from "path"
+import pkg from "../package.json"
 import { buildCommand } from "./commands/buildCommand"
 import { devCommand } from "./commands/devCommand"
 import { lintCommand } from "./commands/lintCommand"
-import { loadConfig } from "./features/config-file/loadConfig"
 import { checkPackages } from "./features/extraneous-packages/checkPackages"
 import { checkSSLConfig } from "./features/ssl-check/checkSSLConfig"
 import { checkForTypeScript } from "./features/typescript/checkForTypeScript"
@@ -11,12 +10,7 @@ import { output } from "./util/output"
 import { runSteps } from "./util/runSteps"
 
 const program = new Command()
-program.version(
-	// eslint-disable-next-line
-	require(path.join(__dirname, "../package.json")).version,
-	"-v, --version",
-	"output the version number"
-)
+program.version(pkg.version, "-v, --version", "output the version number")
 
 program
 	.command("dev")
@@ -33,13 +27,12 @@ program
 	.description("bundles your code for production")
 	.option("-a, --analyze", "analyze your bundle size", false)
 	.action(async (options: { analyze: boolean }) => {
-		const config = await loadConfig()
-
 		try {
-			await buildCommand({ analyze: options.analyze, config })
+			await runSteps([buildCommand({ analyze: options.analyze })])
 		} catch (e) {
 			output.error(e)
 		}
+
 		console.info("")
 	})
 
--- packages/chayns-toolkit/src/commands/buildCommand.ts
+++ packages/chayns-toolkit/src/commands/buildCommand.ts
@@ -1,98 +1,102 @@
 import chalk from "chalk"
 import Table from "cli-table"
-import { ChaynsScriptsConfiguration } from "../features/config-file/configSchema"
 import { createWebpackCompiler } from "../util/createWebpackCompiler"
 import { fm } from "../util/format"
 import { output } from "../util/output"
+import { StepParams } from "../util/runSteps"
 
 interface BuildOptions {
-	config: ChaynsScriptsConfiguration
 	analyze: boolean
 }
 
-export function buildCommand({ config, analyze }: BuildOptions): Promise<void> {
-	return new Promise((resolve, reject) => {
-		process.env.BABEL_ENV = "production"
-		process.env.NODE_ENV = "production"
+export function buildCommand({
+	analyze,
+}: BuildOptions): (stepParams: StepParams) => Promise<void> {
+	return ({ config, packageJson }) => {
+		return new Promise((resolve, reject) => {
+			process.env.BABEL_ENV = "production"
+			process.env.NODE_ENV = "production"
 
-		const compiler = createWebpackCompiler({
-			mode: "production",
-			analyze,
-			outputFilename: config.output.filename,
-			singleBundle: config.output.singleBundle,
-			path: config.output.path,
-		})
+			const compiler = createWebpackCompiler({
+				mode: "production",
+				analyze,
+				outputFilename: config.output.filename,
+				singleBundle: config.output.singleBundle,
+				path: config.output.path,
+				packageJson,
+			})
 
-		const startTime = Date.now()
+			const startTime = Date.now()
 
-		output.info(`Bundling your code...`)
+			output.info(`Bundling your code...`)
 
-		compiler.run((err, stats) => {
-			const buildTime = (Date.now() - startTime) / 1000
-			if (err) return reject(err)
+			compiler.run((err, stats) => {
+				const buildTime = (Date.now() - startTime) / 1000
+				if (err) return reject(err)
 
-			const hasErrors = stats.hasErrors()
+				const hasErrors = stats.hasErrors()
 
-			if (hasErrors) {
-				output.error("Compilation failed.\n")
+				if (hasErrors) {
+					output.error("Compilation failed.\n")
 
-				stats.compilation.errors.forEach((error: Error) => {
-					console.info(error.message)
-				})
+					stats.compilation.errors.forEach((error: Error) => {
+						console.info(error.message)
+					})
+					return resolve()
+				}
+
+				const { assets } = stats.toJson()
+
+				output.info(
+					`Finished build in ${fm.number(
+						buildTime.toFixed(2)
+					)} seconds. These files were emitted:`
+				)
+
+				console.info("")
+
+				if (assets) {
+					const table = new Table({
+						...tableStyles,
+						colAligns: ["left", "right"],
+					})
+
+					assets.forEach((asset) => {
+						let unit = "B"
+						let amount = asset.size
+
+						if (asset.size > 1_000_000) {
+							unit = "mB"
+							amount = asset.size / 1_000_000
+						} else if (asset.size > 1_000) {
+							unit = "kB"
+							amount = asset.size / 1_000
+						}
+
+						const size = `${amount.toLocaleString("en-US", {
+							maximumFractionDigits: 2,
+						})} ${unit}`
+
+						let symbol = " "
+
+						if (asset.size > 500_000) {
+							symbol = chalk.yellow("!")
+						}
+
+						if (asset.size > 1_000_000) {
+							symbol = chalk.redBright("!")
+						}
+
+						table.push([asset.name, size, symbol])
+					})
+
+					console.info(table.toString())
+				}
+
 				return resolve()
-			}
-
-			const { assets } = stats.toJson()
-
-			output.info(
-				`Finished build in ${fm.number(
-					buildTime.toFixed(2)
-				)} seconds. These files were emitted:`
-			)
-
-			console.info("")
-
-			if (assets) {
-				const table = new Table({
-					...tableStyles,
-					colAligns: ["left", "right"],
-				})
-
-				assets.forEach((asset) => {
-					let unit = "B"
-					let amount = asset.size
-
-					if (asset.size > 1_000_000) {
-						unit = "mB"
-						amount = asset.size / 1_000_000
-					} else if (asset.size > 1_000) {
-						unit = "kB"
-						amount = asset.size / 1_000
-					}
-
-					const size = `${amount.toLocaleString("en-US", {
-						maximumFractionDigits: 2,
-					})} ${unit}`
-
-					let symbol = " "
-
-					if (asset.size > 500_000) {
-						symbol = chalk.yellow("!")
-					}
-
-					if (asset.size > 1_000_000) {
-						symbol = chalk.redBright("!")
-					}
-
-					table.push([asset.name, size, symbol])
-				})
-
-				console.info(table.toString())
-			}
-
-			return resolve()
+			})
 		})
-	})
+	}
 }
 
 const tableStyles = {
