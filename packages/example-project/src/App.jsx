1051d4110499d7f0e7e1d7884b2387d5ad20db80
Leonhard Driesch <leo.driesch@gmail.com>
2020-10-16 09:28:53 +0200

    :white_check_mark: Add test component for environment variables

--- packages/example-project/src/App.jsx
+++ packages/example-project/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import styles from "./app.module.css"
 import CorejsTest from "./components/corejs-test/CorejsTest"
 import CssModules from "./components/css-modules/CssModules"
+import EnvTest from "./components/env-test/EnvTest"
 import FileLoaderTest from "./components/file-loader-test/FileLoaderTest"
 import HmrTest from "./components/hmr-test/HmrTest"
 import SvgTest from "./components/svg-test/SvgTest"
@@ -18,6 +19,7 @@ export default function App() {
 			<SvgTest />
 			<TsTest />
 			<CorejsTest />
+			<EnvTest />
 		</div>
 	)
 }
