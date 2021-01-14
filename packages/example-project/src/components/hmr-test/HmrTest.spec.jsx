ba99c5b3f53286b17598769870afb997f353a2eb
Michael Braun <michael.braun@tobit.software>
2021-01-14 09:32:23 +0100

    :sparkles: :construction: Add basic component test with react-testing-library

---
+++ packages/example-project/src/components/hmr-test/HmrTest.spec.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+
+global.window.chayns = {
+    env: {
+        language: 'de',
+        site: {
+            color: '#000000',
+        },
+    },
+};
+
+const HmrTest = require("./HmrTest").default;
+
+const test = true;
+
+
+// checking if counter is working
+it('HmrTest - click', () => {
+    render(<HmrTest />);
+
+    screen.debug();
+
+    const node = screen.getByText('I\'ve been clicked 0 times.');
+    fireEvent.click(node);
+    screen.getByText('I\'ve been clicked 1 times.');
+
+    expect(test).toBe(true);
+});
