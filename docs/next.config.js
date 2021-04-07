c4d49022ab6e21d3f5140192c4f1ca0b76662912
Leonhard Driesch <leo.driesch@gmail.com>
2021-04-07 17:05:59 +0200

    :wrench: Activate webpack5 for docs

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,3 +1,7 @@
 const withNextra = require("nextra")("nextra-theme-docs", "./theme.config.js")
 
-module.exports = withNextra()
+module.exports = withNextra({
+	future: {
+		webpack5: true,
+	},
+})
