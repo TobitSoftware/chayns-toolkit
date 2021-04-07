de813ddb6a8f0e0bedc392734308fddec3353ae3
Leonhard Driesch <leo.driesch@gmail.com>
2021-04-07 10:02:23 +0200

    Revert ":fire: Remove unneccesary TypeScript check in ESLint config"
    This reverts commit 1133f1661e708721e3bf9e06d2dd177b5f4cd257.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,6 +1,15 @@
+const path = require("path")
 const javascriptRules = require("./rules/javascriptRules")
 const sharedRules = require("./rules/sharedRules")
 const typescriptRules = require("./rules/typescriptRules")
+
+// eslint-disable-next-line import/no-dynamic-require
+const packageJson = require(path.resolve("package.json"))
+
+const usesTypeScript = Object.keys({
+	...packageJson.dependencies,
+	...packageJson.devDependencies,
+}).includes("typescript")
 
 module.exports = {
 	env: {
@@ -18,7 +27,7 @@ module.exports = {
 		"import/extensions": [".js", ".jsx", ".ts", ".tsx"],
 	},
 	overrides: [
-		{
+		usesTypeScript && {
 			files: ["**/*.ts?(x)"],
 			extends: [
 				"airbnb-typescript",
