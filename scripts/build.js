62be7b42577f4bf23ec720751a6d97b2df953bfb
Leo Driesch <leonhard.driesch@tobit.software>
2021-07-23 14:22:48 +0200

    :bug: Fix build script

--- scripts/build.js
+++ scripts/build.js
@@ -7,11 +7,3 @@ require("esbuild").buildSync({
 	...esbuildConfig,
 	minify: true,
 })
-
-require("esbuild").buildSync({
-	entryPoints: ["src/eslint/index.js"],
-	bundle: true,
-	platform: "node",
-	target: ["node12"],
-	outfile: "lib/eslint.js",
-})
