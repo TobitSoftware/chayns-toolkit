924ffd8d90ee2da6f4d228746e22859e28799636
Heiner Pöpping <heiner.poepping@tobit.software>
2022-10-21 11:50:45 +0200

    :bug: Fix possible issue siteId in chayns css url

--- src/util/loadChaynsCss.ts
+++ src/util/loadChaynsCss.ts
@@ -4,8 +4,12 @@
 // Replaced compatibility script
 // Should be removed when styles are in chayns-components and/or in static style css
 export const loadCss = () => {
-	const apiUrl = "https://api.chayns.net/css/"
+	const apiUrl = new URL("https://api.chayns.net/css/")
 	const parameters = new URLSearchParams(window.location.search.toLowerCase())
+
+	if (!apiUrl.searchParams.has("siteId")) {
+		apiUrl.searchParams.set("siteId", parameters.get("siteid") || "")
+	}
 
 	document.documentElement.style.visibility = "hidden"
 	document.documentElement.style.overflow = "hidden"
@@ -15,7 +19,7 @@ export const loadCss = () => {
 
 	link.rel = "stylesheet"
 	link.type = "text/css"
-	link.href = apiUrl + (parameters.get("siteid") || "")
+	link.href = apiUrl.toString()
 	link.onload = () => {
 		document.documentElement.style.visibility = "visible"
 	}
