a9de6deadda6f1224274ccf7ec2a7cad67550770
Marius Gertdenken <marius.gertdenken@tobit.software>
2023-01-16 15:37:10 +0100

    Change browserslist, do not support android < 5

--- src/features/environment/browserslist.ts
+++ src/features/environment/browserslist.ts
@@ -4,7 +4,13 @@ export const devBrowsers = [
 	"last 3 safari version",
 ].join()
 
-export const prodBrowsers = ["cover 90%", "not dead", "not op_mini all", "Firefox ESR"].join()
+export const prodBrowsers = [
+	"cover 90%",
+	"not dead",
+	"not op_mini all",
+	"Firefox ESR",
+	"not android < 5",
+].join()
 
 export function setBrowserslistEnvironment(mode: "development" | "production" | "none"): void {
 	if (mode === "development") {
