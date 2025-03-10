e673ced4881b519a65b7054fe1810e416972ee2e
Heiner Pöpping <heiner.poepping@tobit.com>
2025-03-10 14:47:31 +0100

    Move rspack/core to devDependencies

--- src/util/webpackPromises.ts
+++ src/util/webpackPromises.ts
@@ -1,19 +1,7 @@
 import { RsbuildInstance } from "@rsbuild/core"
-import { Compiler, Stats } from "@rspack/core"
+import type { Stats } from "@rspack/core"
 
 export async function runCompiler(rsbuild: RsbuildInstance): Promise<Stats> {
 	const result = await rsbuild.build()
 	return result.stats as Stats
-}
-
-export function closeCompiler(compiler: Compiler): Promise<void> {
-	return new Promise((resolve, reject) => {
-		compiler.close((error) => {
-			if (error) {
-				reject(error)
-			} else {
-				resolve()
-			}
-		})
-	})
 }
